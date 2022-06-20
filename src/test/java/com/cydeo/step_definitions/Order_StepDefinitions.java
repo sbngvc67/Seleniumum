0691642541ebde30538534fdf3ba8cfaf2b066d0
sbngvc67 <sbngvc67@hotmail.com>
2022-06-20 13:57:11 +0200

    Third Commit Selenium

--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -72,31 +72,23 @@ public class Order_StepDefinitions {
 
     @When("user selects credit card type {string}")
     public void user_selects_credit_card_type(String expectedCardType) {
-        List<WebElement> cardTypes=orderPage.cardType;
 
-//        for (WebElement each : cardTypes) {
-//
-//            if (each.getAttribute("value").equalsIgnoreCase(expectedCardType)){
-//                each.click();
-//            }
-//        }
-
-        BrowserUtils.clickRadioButton(cardTypes,expectedCardType);
+        BrowserUtils.clickRadioButton(orderPage.cardType,expectedCardType);
     }
 
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-
+        orderPage.cardNoInput.sendKeys(string);
     }
 
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-
+        orderPage.cardExpInput.sendKeys(string);
     }
 
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-
+        orderPage.processOrderBtn.click();
     }
 
     @Then("user should see {string} in first row of the web table")
